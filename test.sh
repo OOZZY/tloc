9eb9866bc268afce9441db67b2c2ac3442a854da
oozzy <oozzy.tlo@gmail.com>
2015-03-20 17:37:05 -0700

    test.sh: change order of commands so that all compilation is done first

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ set -xe
 
 clang -std=c11 -pedantic -Wall -Wextra -Werror -g -I. darray_test.c tlo/*.c -o darray_test
 gcc -std=c11 -pedantic -Wall -Wextra -Werror -g -I. darray_test.c tlo/*.c -o darray_test-gcc
-valgrind --tool=memcheck --leak-check=yes --track-origins=yes ./darray_test
-
 clang -std=c11 -pedantic -Wall -Wextra -Werror -g -I. sllist_test.c tlo/*.c -o sllist_test
 gcc -std=c11 -pedantic -Wall -Wextra -Werror -g -I. sllist_test.c tlo/*.c -o sllist_test-gcc
+
+valgrind --tool=memcheck --leak-check=yes --track-origins=yes ./darray_test
 valgrind --tool=memcheck --leak-check=yes --track-origins=yes ./sllist_test
