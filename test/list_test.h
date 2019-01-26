71ad62019f76e9a277f5ecd23a4acce80d15a400
oozzy <oozzy.tlo@gmail.com>
2019-01-25 17:15:44 -0800

    Move tests in list_test module to tloc_test.c

--- test/list_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_LIST_TEST_H
-#define TEST_LIST_TEST_H
-
-void testList(void);
-
-#endif  // TEST_LIST_TEST_H
--- test/tloc_test.c
+++ test/tloc_test.c
@@ -4,9 +4,16 @@
 #include "cdarray_test.h"
 #include "darray_test.h"
 #include "dllist_test.h"
-#include "list_test.h"
+#include "list_test_utils.h"
 #include "sllist_test.h"
 #include "statistics_test.h"
+
+static void testList(void) {
+  testListDeleteWithNull();
+  puts("================");
+  puts("List tests done.");
+  puts("================");
+}
 
 int main(void) {
   TloStopwatch stopwatch;
